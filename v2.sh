ad0afa151acffb1ef35e0cb21da55f0f35b608c7
slecth <cloud20222004@gmail.com>
2025-06-07 05:24:12 -0300

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Qual seu backend v2ray? " vaga
+read -p "Qual seu apontamento v2ray? ex: 5 " vaga
 
 if [[ ! "$vaga" =~ ^[0-9]+$ ]]; then
     echo "Erro: Você deve inserir um número válido."
@@ -9,4 +9,4 @@ fi
 
 sed -i "s|\"path\": \"/ws/\"|\"path\": \"/ws$vaga/\"|g" /etc/v2ray/config.json && systemctl restart v2ray
 
-echo "Configuração alterada para o backend $vaga e V2Ray reiniciado!"
+echo "Configuração alterada e V2Ray reiniciado!"
